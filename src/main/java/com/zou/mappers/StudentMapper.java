e020199679afca7cc60e88c5e7b276df9dc3a2ed
zoulvzhou <940933073@qq.com>
2018-05-01 11:17:04 +0800

    增删改查

--- src/main/java/com/zou/mappers/StudentMapper.java
+++ src/main/java/com/zou/mappers/StudentMapper.java
@@ -2,7 +2,17 @@ package com.zou.mappers;
 
 import com.zou.model.Student;
 
+import java.util.List;
+
 public interface StudentMapper {
 
     public int add(Student student);
+
+    public int update(Student student);
+
+    public int delete(Integer id);
+
+    public Student findById(Integer id);
+
+    public List<Student> findAll();
 }
